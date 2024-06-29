c61a8b8f63b4c54acf59a509605e29d5f28a4233
ArchitKayal <59079120+ArchitKayal@users.noreply.github.com>
2024-06-29 19:35:14 +0530

    Created 1989 A. Catch the Coin.cpp

---
+++ 1989 A. Catch the Coin.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+ 
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+ 
+using namespace std;
+ 
+void solve()
+{
+    int x,y;
+    cin>>x>>y;
+    cout<<(y>=-1?"YES":"NO")<<endl;
+}
+ 
+int32_t main(){
+ 
+    int t = 1;
+    cin>>t;
+    while(t--) solve();
+ 
+    return 0;
+}
